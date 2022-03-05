2b960daa651ead5490926b29967b7d8cfcc016d6
aleksandrsokolov <timac11@bk.ru>
2022-03-05 10:16:27 +0300

    update proceedings

--- src/components/Proceedings.jsx
+++ src/components/Proceedings.jsx
@@ -5,24 +5,12 @@ export default function Proceedings() {
     <>
       <div className="md:leading-7 md:text-xl">
         <p className="">
-          We are pleased to announce that one of the DCCN 2021 post-proceedings books (LNCS, volume 13144) has been published by <span className="font-extrabold text-indigo-500">Springer Nature</span>.
-          The book is available online. The other book (CCIS, volume 1552) is planned to be published in 2022.
+          We are pleased to announce that both of the DCCN 2021 post-proceedings books (LNCS, volume 13144, and CCIS, volume 1552) has been published by Springer Nature.
+          The books are available online.
         </p>
-        {/*<div className="flex">*/}
-        {/*  <div className="mr-5" style={{width: "100px"}}>*/}
-        {/*    <img src="/images/ccis_cover.jpg" alt="CCIS Volume cover"/>*/}
-        {/*  </div>*/}
-        {/*  <div>*/}
-        {/*    <p className="font-semibold">*/}
-        {/*      CCIS 1337:*/}
-        {/*      <br />*/}
-        {/*      <a className="text-blue-500" href="https://www.springer.com/978-3-030-66241-7">https://www.springer.com/978-3-030-66241-7</a>*/}
-        {/*    </p>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
         <div className="flex mt-6">
-          <div className="mr-5" style={{width: "200px", minWidth: "120px"}}>
-            <img src="/images/lncs_cover.jpg" alt="LNCS Volume cover"/>
+          <div className="mr-5" style={{maxWidth: "140px", minWidth: "120px"}}>
+            <img src="/images/lncs_cover.png" alt="LNCS Volume cover"/>
           </div>
           <div style={{wordBreak: "break-word"}}>
             <p className="font-semibold">
@@ -32,6 +20,22 @@ export default function Proceedings() {
             </p>
             <p className="mt-8">
               The conference participants are granted the four weeks free online access to the e-book of the LNCS proceedings.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex mt-6">
+          <div className="mr-5" style={{maxWidth: "140px", minWidth: "120px"}}>
+            <img src="/images/ccis_cover.png" alt="CCIS Volume cover"/>
+          </div>
+          <div style={{wordBreak: "break-word"}}>
+            <p className="font-semibold">
+              CCIS 1552:
+              <br />
+              <a className="text-blue-500" target="_blank" href="https://link.springer.com/book/10.1007/978-3-030-97110-6">https://link.springer.com/book/10.1007/978-3-030-97110-6</a>
+            </p>
+            <p className="mt-8">
+              The access to the CCIS volume is granted for the period from March 7 to April 7, 2022.
             </p>
           </div>
         </div>
